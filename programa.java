3c1a1eb41900026d1571c13eb6705d2c8a0fbf9e
Plácido Mota <38774223+placidomota@users.noreply.github.com>
2021-10-19 16:24:25 -0500

    Atualização do código

--- programa.java
+++ programa.java
@@ -1,6 +1,7 @@
 public class Programa {
+  
   public static void main (String[] args) {
-    System.out.println("Meu primeiro programa em java")
+    System.out.println("Meu primeiro programa em java");
     
     }
 }
